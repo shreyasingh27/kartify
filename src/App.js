3103c6471036e95afa3bdd69d22f3b7913a98c4f
System Administrator <root@ANSs-MacBook-Pro.local>
2019-08-08 14:15:49 +0530

    collection preview and collection item page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {Route,Switch} from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+
+const Hats = () =>(
+    <h1>Hats Page</h1>
+)
 
 function App() {
   return (
-    <div>
-    <Homepage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route path='/shop' component={ShopPage}/>
+    </Switch>
+    
   );
 }
 
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item';
+
+const CollectionPreview =({title,items,id})=>(
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+    {
+        items.filter((item,idx)=> idx< 4).map(({id,...otherCollectionProps})=>(
+           <CollectionItem  id = {id}{...otherCollectionProps}/>
+        ))
+    }
+    </div>
+    </div>
+)
+export default CollectionPreview;
+
